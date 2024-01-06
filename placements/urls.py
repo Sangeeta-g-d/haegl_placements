515b2a6d9ae30481993d64992792874112580fb4
sangeeta-g-d <sangeetadani599@gmail.com>
2024-01-06 13:28:42 +0530

    company login

--- placements/urls.py
+++ placements/urls.py
@@ -5,9 +5,11 @@ from django.contrib import admin
 urlpatterns = [
 
     path('admin_db',views.admin_db,name='admin_db'),
-    path('login',views.login,name='login'),
+    path('login',views.login1,name='login'),
+    path('update_status',views.update_status,name='update_status'),
     path('registration',views.registration,name='registration'),
     path('admin_login',views.admin_login,name='admin_login'),
     path('admin_logout',views.admin_logout,name='admin_logout'),
+    path('company_dashboard',views.company_dashboard,name="company_dashboard")
 
     ]
